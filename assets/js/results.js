e92fa865e9e9abacf61618546a7d46cd49383208
Caleb <bambini6@gmail.com>
2022-09-27 01:20:16 -0500

    Added functionality to render the ingredients correctly, and the instructions.

--- assets/js/results.js
+++ assets/js/results.js
@@ -22,21 +22,32 @@ document.addEventListener('DOMContentLoaded', function() {
       var mealName = mainArr.strMeal;
       var instructions = mainArr.strInstructions;
       var measures = [mainArr.strMeasure1, mainArr.strMeasure2, mainArr.strMeasure3, mainArr.strMeasure4, mainArr.strMeasure5, mainArr.strMeasure6, mainArr.strMeasure7, mainArr.strMeasure8, mainArr.strMeasure9, mainArr.strMeasure10, mainArr.strMeasure11, mainArr.strMeasure12, mainArr.strMeasure13, mainArr.strMeasure14, mainArr.strMeasure15, mainArr.strMeasure16, mainArr.strMeasure17, mainArr.strMeasure18, mainArr.strMeasure19, mainArr.strMeasure20,]
-      // console.log(instructions)
+      var ingredients = [mainArr.strIngredient1, mainArr.strIngredient2, mainArr.strIngredient3, mainArr.strIngredient4, mainArr.strIngredient5, mainArr.strIngredient6, mainArr.strIngredient7, mainArr.strIngredient8, mainArr.strIngredient9, mainArr.strIngredient10, mainArr.strIngredient11, mainArr.strIngredient12, mainArr.strIngredient13, mainArr.strIngredient14, mainArr.strIngredient15, mainArr.strIngredient16, mainArr.strIngredient17, mainArr.strIngredient18, mainArr.strIngredient19, mainArr.strIngredient20]
+      var combined = measures.map((e, i) => e + " " + ingredients[i]);
 
-      foodPic.setAttribute("src", mealImg );
-      document.querySelector("#recipename").textContent = mealName
-      
-      console.log(measures)
+
+        
+      console.log(combined)
       var ingList = document.createElement("ul")
+      var inst = document.createElement("p");
+      inst.textContent = instructions
+      document.querySelector("#mealins").appendChild(inst)
       ingList.setAttribute("id", "inglist")
       document.querySelector("#mealingrs").appendChild(ingList)
 
-      
-    })
+      combined.forEach(element => {
+        if(element !== " "){
+        console.log(element.split()) 
+        var ingItem = document.createElement("li");
+        ingItem.textContent = element
+        ingList.appendChild(ingItem)
+        };
 
-    
 
-    // 
-
+      });
+      // console.log(ingredients)
+      // console.log(measures)
+      foodPic.setAttribute("src", mealImg );
+      document.querySelector("#recipename").textContent = mealName      
+    });
 });
