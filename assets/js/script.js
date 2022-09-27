dad14fcc0ba3a988cf1f8035d9fc1163ad195f41
Caleb <bambini6@gmail.com>
2022-09-26 21:46:39 -0500

    added functionality of fetch to results page.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,14 @@
-var recipeCardImgs = (document.querySelectorAll(".randImg"));
-var recipeCardNames = document.querySelectorAll(".mealName")
+var recipeCardImgs = document.querySelectorAll(".randImg");
+var recipeCardNames = document.querySelectorAll(".mealName");
 var recipeCardCat = document.querySelectorAll(".mealCat");
+var recipeCard = document.querySelectorAll(".card");
 
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems);
-    
-
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".sidenav");
+  var instances = M.Sidenav.init(elems);
 });
 
- 
- 
-
-  fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+fetch("https://www.themealdb.com/api/json/v1/1/random.php")
   .then(function (resp) {
     return resp.json();
   }) // Convert data to json
@@ -23,31 +19,33 @@ document.addEventListener('DOMContentLoaded', function() {
     // console.log(data.meals[0].strCategory);
     // console.log(data.meals[0].strMeal);
 
-    
     var mealImg = data.meals[0].strMealThumb;
     var mealID = data.meals[0].idMeal;
     var mealCat = data.meals[0].strCategory;
     var mealName = data.meals[0].strMeal;
-    
-  
-    recipeCardImgs.forEach(element => {
-      console.log(element)
-      element.setAttribute("src", mealImg);       
+
+    recipeCardImgs.forEach((element) => {
+      console.log(element);
+      element.setAttribute("src", mealImg);
     });
 
-    recipeCardNames.forEach(element => {
-      console.log(element)
-      element.textContent = mealName       
+    recipeCardNames.forEach((element) => {
+      console.log(element);
+      element.textContent = mealName;
     });
 
-    recipeCardCat.forEach(element => {
-      console.log(element)
-      element.textContent = mealCat       
+    recipeCardCat.forEach((element) => {
+      console.log(element);
+      element.textContent = mealCat;
     });
 
+    recipeCard.forEach((element) => {
+      element.setAttribute("data", mealID);
 
-
-
+      element.addEventListener("click", function (event) {
+        var currentId = event.currentTarget.getAttribute("data");
+        window.location.href = "./results.html?currentID="+currentId
+      });
+      
     });
-
-    
+  });
